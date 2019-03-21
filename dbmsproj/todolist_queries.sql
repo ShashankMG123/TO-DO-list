b47c566623d7164500a3d7052a5270abe9500c91
Shashank MG <37444395+ShashankMG123@users.noreply.github.com>
2019-03-21 19:18:19 +0530

    comments added

--- dbmsproj/todolist_queries.sql
+++ dbmsproj/todolist_queries.sql
@@ -1,49 +1,62 @@
+/*
+SELECT * from department;
+SELECT * from student;
+SELECT * from teachers;
+SELECT * from groups;
+*/
+
+/* selects groups that a student is a part of*/
 SELECT GID,joindate
-FROM GROUPS AS G, STUDENT AS S
-WHERE G.SRN=S.SRN AND SNAME='Abhaay';
+from student as s, groups as g
+where s.srn = g.srn and sname = 'Abhaay';
+
+/* select groups that a teacher controlls and the number of students in that group*/
+select g.gid, count(*) as number_of_students
+from    groups as g
+group by g.tname,g.gid
+having g.tname='Balaji';
+
+/* view all teachers in your department (as teacher)*/
+select tname, dname
+from teachers as t, department as d
+where  t.tname != 'Prakash' and d.did = t.tdid and d.did in 
+(
+    select tdid
+    from teachers
+    where tname = 'Prakash'
+);
+
+/* view all student in your department (as teacher)*/
+select sname, dname
+from department as d, student as s
+where  s.sdid = d.did and d.did in 
+(
+    select tdid
+    from teachers
+    where tname = 'Prakash'
+);
 
 
-
-SELECT gid, count(*) 
-FROM groups 
-GROUP BY tname, gid
-HAVING tname='Giridhar'; 
-
-
-
-select tname,dname
-from teachers,department
-where tname!='Giridhar' and did=tdid and did in (select tdid
-                                                from teachers
-                                                where tname='Giridhar');
-
-
-select sname,dname
-from student,department
-where sdid=did and did in (select tdid
-                            from teachers
-                            where tname='Maheshwari');
-
-
-select tname,dname
-from teachers,department
-where tdid=did and did in (select sdid
-                            from student
-                            where sname='Archana');
-
-
-select dname,no_of_groups
-from department,(select count(gid) as no_of_groups
-                from (select distinct gid
-                from groups
-                group by tname,gid
-                having tname in (select tname
-                from teachers
-                where tdid='8'))as x )as y
-where did='8';
-
+/* view all teachers in your department (as student) */
+select tname
+from student, teachers
+where sname = 'Shreya' and sdid = tdid;
  
+ 
+/* count number of groups under a particuar department */
 
-
-
-
+select dname,number_of_groups
+from department,(
+    select count(gid) as number_of_groups
+    from (
+        select distinct gid
+        from groups
+        group by tname,gid
+        having tname in  (
+            select tname
+            from teachers
+            where tdid = '8'
+        )
+    ) as x
+) as y
+where did = '8';
